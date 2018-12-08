c8861c45f2e76a8c60cae8c974bb32dac84c4b68
laxel <axeka@student.chalmers.se>
2018-12-08 13:06:53 +0100

    Part1/2 done :)

--- AOC/Day5/d5.java
+++ AOC/Day5/d5.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class d5 {
 
     public static void main(String[] args)throws Exception {
+        // Read input
         File file = new File("input.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
 
@@ -14,20 +15,51 @@ public class d5 {
             if((int)c != 10) list.add((Character)(char)c);
         }
 
+        part1(list);
+        part2(list);
+    }
+    public static void part1(ArrayList<Character> list) {
         // -------------------- PART 1 --------------------
+        ArrayList<Character> listCopy = new ArrayList<Character>();
+        for(Character c : list) listCopy.add(c);
 
-        ArrayList<Character> reactedList = polymerReaction(list);
+        polymerReaction(listCopy);
 
         System.out.println("/-------------- PART 1 --------------/");
-        System.out.println("Size: " + reactedList.size());
-
-        // -------------------- PART 2 --------------------
-
+        System.out.println("Size: " + listCopy.size());
     }
 
-    public static ArrayList<Character> polymerReaction(ArrayList<Character> _list) {
-        ArrayList<Character> list = new ArrayList<Character>();
-        for(Character c : _list) list.add(c);
+    public static void part2(ArrayList<Character> list) {
+        // -------------------- PART 2 --------------------
+        // Find all letters in input:
+        HashSet<Character> letters = new HashSet<Character>();
+        for(Character ch : list) {
+            Character upperCaseCh = (Character)Character.toUpperCase(ch);
+            letters.add(upperCaseCh);
+        }
+
+        // Try to react individually with each letter found in previus step.
+        int minSize = Integer.MAX_VALUE;
+        Character bestChar = ' ';
+        for(Character ch : letters) {
+            // Copy original list
+            ArrayList<Character> listCopy = new ArrayList<Character>();
+            for(Character c : list) listCopy.add(c);
+            // Remove chosen letter
+            removeLetter(listCopy, ch);
+            // Do the reaction on the list
+            polymerReaction(listCopy);
+            if(listCopy.size() < minSize) {
+                minSize = listCopy.size();
+                bestChar = ch;
+            }
+
+        }
+        System.out.println("/-------------- PART 2 --------------/");
+        System.out.println("The 'best' character was " + bestChar + " with size " + minSize);
+    }
+
+    public static void polymerReaction(ArrayList<Character> list) {
 
         boolean removedItem = false;
         int numRemoves = 0;
@@ -58,7 +90,15 @@ public class d5 {
                 }
             }
         } while(removedItem && list.size() > 1);
-        return list;
     }
 
+    public static void removeLetter(ArrayList<Character> list, Character letter) {
+        for(int i = 0; i < list.size(); i++) {
+            Character UpperCaseCh = (Character)Character.toUpperCase(list.get(i));
+            if(letter.equals(UpperCaseCh)) {
+                list.remove(i);
+                i--;
+            }
+        }
+    }
 }

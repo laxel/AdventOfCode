c2e1d2bd75a6f6b8b7e7422534b3c38d7369a055
laxel <axeka@student.chalmers.se>
2018-12-10 18:21:17 +0100

    Day9, part1 done

--- AOC/Day9/d9.java
+++ AOC/Day9/d9.java
@@ -7,14 +7,30 @@ public class d9 {
         File file = new File("input.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
 
-        String str;
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        String str = br.readLine();
+        int numPlayers = Integer.parseInt(str.split(" ")[0]);
+        int maxMarble = Integer.parseInt(str.split(" ")[6]);
 
-        while ((str = br.readLine()) != null) {
-            String[] strList = str.split(" ");
-            for(String s : strList) {
-                list.add(Integer.parseInt(s));
+        MarbleList list = new MarbleList();
+        int[] playerScore = new int[numPlayers];
+        int currPlayer = 1;
+        int currValue = 1;
+        while(currValue < maxMarble) {
+            if(currValue % 23 == 0) {
+                list.goCCW(7);
+                playerScore[currPlayer - 1] += currValue + list.remove();
+            } else {
+                list.goCW(2);
+                list.insert(currValue);
             }
+            currValue++;
+            currPlayer++;
+            if(currPlayer > numPlayers) currPlayer = 1;
         }
+        int maxScore = 0;
+        for(int i = 0; i < playerScore.length; i++) {
+            if(playerScore[i] > maxScore) maxScore = playerScore[i];
+        }
+        System.out.print("Max score was: " + maxScore);
     }
 }

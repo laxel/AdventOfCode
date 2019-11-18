c9646baa25a7f18caf829ef4660b63eb9b550bde
Axel <axeka@student.chalmers.se>
2019-11-18 12:31:56 +0100

    day 5 done

--- 2015/d05/d05.py
+++ 2015/d05/d05.py
@@ -13,4 +13,23 @@ for line in f:
 		
 print("Part 1:",numNice)
 
+f = open("input.txt")
+
 # Part 2
+numNice = 0
+for line in f:
+	cond1 = False
+	cond2 = False
+	
+	groups = [line[i] + line[i+1] for i in range(0,len(line)-1)]
+	for j in range(1,len(groups)):
+		overlapList = groups[0:j-1] + groups[j+2:]
+		if groups[j] in overlapList:
+			cond1 = True
+			break
+	
+	cond2 = any([line[i-1] == line[i+1] for i in range(1,len(line)-1)])
+		
+	if cond1 and cond2:
+		numNice += 1
+print(numNice)

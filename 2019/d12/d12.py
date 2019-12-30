58afcd69fbe018d0782a1d3c0b58ce2917b49fa6
Axel Karlsson <axeka@student.chalmers.se>
2019-12-30 22:45:16 +0100

    19-12 part 1

---
+++ 2019/d12/d12.py
@@ -0,0 +1,67 @@
+import re
+f = open("input.txt")
+moons = []
+steps = 1000
+
+# Parse input
+for l in f:
+	coords = re.findall("-?\d+", l)
+	moons.append([[int(x) for x in coords],[0,0,0]])
+p2Moons = moons.copy()
+# --- Part 1 ---
+
+# Caluclate moon change
+for _ in range(0,steps):
+	# Calculate new velocity
+	for i in range(0,len(moons)):
+		crM = moons[i]
+		for j in range(0,len(moons)):
+			ckM = moons[j]
+			if i != j:
+				crM[1][0] += 0 if crM[0][0] == ckM[0][0] else 1 if crM[0][0] < ckM[0][0] else -1
+				crM[1][1] += 0 if crM[0][1] == ckM[0][1] else 1 if crM[0][1] < ckM[0][1] else -1
+				crM[1][2] += 0 if crM[0][2] == ckM[0][2] else 1 if crM[0][2] < ckM[0][2] else -1
+	# Calculate new coords
+	for m in moons:
+		m[0][0] += m[1][0]
+		m[0][1] += m[1][1]
+		m[0][2] += m[1][2]
+
+# Get total energy
+s = 0
+for m in moons:
+	s += sum([abs(x) for x in m[0]]) * sum([abs(x) for x in m[1]])
+print("Part 1:",s)
+
+
+	
+# --- Part 2 ---
+moons = p2Moons
+prevPositions = set()
+k = 0
+# Caluclate moon change
+while True:
+	# Add and check set
+	coords = tuple([tuple(x[0]) for x in moons])
+	if coords in prevPositions:
+		print(k)
+		break
+	prevPositions.add(coords)
+	k += 1
+	# Calculate new velocity
+	for i in range(0,len(moons)):
+		crM = moons[i]
+		for j in range(0,len(moons)):
+			ckM = moons[j]
+			if i != j:
+				crM[1][0] += 0 if crM[0][0] == ckM[0][0] else 1 if crM[0][0] < ckM[0][0] else -1
+				crM[1][1] += 0 if crM[0][1] == ckM[0][1] else 1 if crM[0][1] < ckM[0][1] else -1
+				crM[1][2] += 0 if crM[0][2] == ckM[0][2] else 1 if crM[0][2] < ckM[0][2] else -1
+	# Calculate new coords
+	for m in moons:
+		m[0][0] += m[1][0]
+		m[0][1] += m[1][1]
+		m[0][2] += m[1][2]
+
+		
+
